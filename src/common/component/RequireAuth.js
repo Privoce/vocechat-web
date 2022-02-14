92d83e0dac0920c12ac835d17030c343e8b47a3f
zerosoul <yanggc888@163.com>
2022-02-14 16:38:53 +0800

    chore: auth redirect with replace

--- src/common/component/RequireAuth.js
+++ src/common/component/RequireAuth.js
@@ -1,9 +1,9 @@
 // import React from 'react'
-import { Navigate } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
 
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
-export default function RequireAuth({ children, redirectTo = "/login" }) {
-  const { token } = useSelector((store) => store.authData);
-  return token ? children : <Navigate to={redirectTo} />;
+export default function RequireAuth({ children, redirectTo = '/login' }) {
+ const { token } = useSelector((store) => store.authData);
+ return token ? children : <Navigate to={redirectTo} replace />;
 }
