834c379e7ccdae8e5a708f984a08109d36b7db23
zerosoul <yanggc888@163.com>
2022-02-28 13:22:53 +0800

    feat: favicon with api domain

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 // import React from 'react';
-import ReactDOM from "react-dom";
-import { Toaster } from "react-hot-toast";
+import ReactDOM from 'react-dom';
+import { Toaster } from 'react-hot-toast';
+import { Helmet } from 'react-helmet';
+import { Reset } from 'styled-reset';
 
-import { Reset } from "styled-reset";
-
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import "animate.css";
-import ReduxRoutes from "./routes";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import 'animate.css';
+import ReduxRoutes from './routes';
+import BASE_URL from './app/config';
 
 ReactDOM.render(
-  <>
-    <Reset />
-    <Toaster />
-    <DndProvider backend={HTML5Backend}>
-      <ReduxRoutes />
-    </DndProvider>
-  </>,
-  document.getElementById("root")
+ <>
+  <Helmet>
+   <link rel="icon" href={`${BASE_URL}/resource/organization/logo`} />
+  </Helmet>
+  <Reset />
+  <Toaster />
+  <DndProvider backend={HTML5Backend}>
+   <ReduxRoutes />
+  </DndProvider>
+ </>,
+ document.getElementById('root')
 );
