a9e06dcc7d1d6985ca72412863d8fb81e163533f
zerosoul <yanggc888@163.com>
2022-03-24 10:08:58 +0800

    chore: drop console in prod

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -179,6 +179,7 @@ module.exports = function (webpackEnv) {
               ecma: 8,
             },
             compress: {
+              drop_console: true,
               ecma: 5,
               warnings: false,
               // Disabled because of an issue with Uglify breaking seemingly valid code:
