c426c7a971f091c3ab8883dca64c54a2056ec61f
zerosoul <yanggc888@163.com>
2022-04-02 12:49:28 +0800

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -20,7 +20,7 @@ firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
-
+console.log("wwwwwwwwww");
 // Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
 messaging.onBackgroundMessage(function (payload) {
   console.log("Received background message ", payload);
