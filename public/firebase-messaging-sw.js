ec58d67e93174ac01cf83bcba9bf809c239ec147
zerosoul <yanggc888@163.com>
2022-04-14 21:30:29 +0800

    fix: duplicated notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,45 @@
 // This a service worker file for receiving push notifitications.
 // See `Access registration token section` @ https://firebase.google.com/docs/cloud-messaging/js/client#retrieve-the-current-registration-token
 
+self.addEventListener("notificationclick", function (event) {
+  console.log("notification click", event, event.notification);
+  event.waitUntil(
+    (async function () {
+      const allClients = await clients.matchAll({
+        includeUncontrolled: true,
+      });
+      const [firstClient] = allClients;
+      // 没有数据
+      const customData = event.notification?.data?.FCM_MSG?.data;
+      if (!customData) {
+        firstClient.focus();
+        return;
+      }
+      const {
+        rustchat_from_uid,
+        rustchat_to_uid,
+        rustchat_to_gid,
+      } = customData;
+
+      let chatClient;
+      let redirectPath = rustchat_to_uid
+        ? `/chat/dm/${rustchat_from_uid}`
+        : rustchat_to_gid
+        ? `/chat/channel/${rustchat_to_gid}`
+        : "";
+      if (!redirectPath) {
+        firstClient.focus();
+        return;
+      }
+      if (allClients.length == 0) {
+        chatClient = await clients.openWindow(redirectPath);
+      } else {
+        firstClient.postMessage({ newPath: redirectPath });
+        firstClient.focus();
+      }
+    })()
+  );
+});
 // Scripts for firebase and firebase messaging
 importScripts(
   "https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"
@@ -24,58 +63,18 @@ firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
-console.log("wwwwwwwwww");
 // Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
 // data:{from_server_id}
-messaging.onBackgroundMessage((payload) => {
-  console.log("Received background message ", payload);
+// messaging.onBackgroundMessage((payload) => {
+//   console.log("Received background message ", payload);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    data: payload.data,
-    body: payload.notification.body,
-  };
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     data: payload.data,
+//     body: payload.notification.body,
+//   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-self.addEventListener("notificationclick", function (event) {
-  console.log("notification click", event, event.notification);
-  event.waitUntil(
-    (async function () {
-      const allClients = await clients.matchAll({
-        includeUncontrolled: true,
-      });
-      const [firstClient] = allClients;
-      // 没有数据
-      if (!event.notification.data) {
-        firstClient.focus();
-        return;
-      }
-      const {
-        rustchat_from_uid,
-        rustchat_to_uid,
-        rustchat_to_gid,
-      } = event.notification.data;
-
-      let chatClient;
-      let redirectPath = rustchat_to_uid
-        ? `/chat/dm/${rustchat_from_uid}`
-        : rustchat_to_gid
-        ? `/chat/channel/${rustchat_to_gid}`
-        : "";
-      if (!redirectPath) {
-        firstClient.focus();
-        return;
-      }
-      if (allClients.length == 0) {
-        chatClient = await clients.openWindow(redirectPath);
-      } else {
-        firstClient.postMessage({ newPath: redirectPath });
-        firstClient.focus();
-      }
-    })()
-  );
-});
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
 // self.addEventListener("notificationclose", function (event) {});
