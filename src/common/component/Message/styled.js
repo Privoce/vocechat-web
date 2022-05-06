bfa83409d5744a8c171f546b125ab0b118dca714
zerosoul <yanggc888@163.com>
2022-05-06 10:19:42 +0800

    chore: update favorite icon in different sences

--- src/common/component/Message/styled.js
+++ src/common/component/Message/styled.js
@@ -1,95 +1,94 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 const StyledMsg = styled.div`
-  position: relative;
+ position: relative;
+ display: flex;
+ align-items: flex-start;
+ gap: 16px;
+ padding: 4px 8px;
+ margin: 8px 0;
+ border-radius: 8px;
+ content-visibility: auto;
+ contain-intrinsic-size: auto 150px;
+ &.in_view {
+  content-visibility: visible;
+ }
+ &[data-highlight='true'] {
+  background: #f5f6f7;
+ }
+ &:hover,
+ &.preview {
+  content-visibility: inherit;
+  contain-intrinsic-size: inherit;
+  background: #f5f6f7;
+  .cmds {
+   visibility: visible;
+  }
+ }
+ &.readonly:hover {
+  background: none;
+ }
+ .avatar {
+  flex-shrink: 0;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  img {
+   width: 100%;
+   height: 100%;
+   border-radius: 50%;
+  }
+ }
+ > .details {
+  width: 100%;
   display: flex;
-  align-items: flex-start;
-  gap: 16px;
-  padding: 4px 8px;
-  margin: 8px 0;
-  border-radius: 8px;
-  content-visibility: auto;
-  contain-intrinsic-size: auto 150px;
-  &.in_view {
-    content-visibility: visible;
+  flex-direction: column;
+  gap: 8px;
+  .up {
+   display: flex;
+   align-items: center;
+   gap: 8px;
+   font-weight: 500;
+   .name {
+    color: #06b6d4;
+    font-style: normal;
+    font-size: 14px;
+    line-height: 20px;
+   }
+   .time {
+    color: #bfbfbf;
+    font-size: 12px;
+    line-height: 18px;
+   }
   }
-  &[data-highlight="true"] {
-    background: #f5f6f7;
-  }
-  &:hover,
-  &.preview {
-    content-visibility: inherit;
-    contain-intrinsic-size: inherit;
-    background: #f5f6f7;
-    .cmds {
-      visibility: visible;
-    }
-  }
-  &.readonly:hover {
-    background: none;
-  }
-  .avatar {
-    flex-shrink: 0;
+  .down {
+   user-select: text;
+   color: #374151;
+   font-weight: normal;
+   font-size: 14px;
+   line-height: 20px;
+   word-break: break-all;
+   white-space: pre-wrap;
+   .edited {
+    margin-left: 5px;
+    color: #999;
+    font-size: 10px;
+   }
+   &.sending {
+    opacity: 0.9;
+   }
+   .img {
+    max-width: 480px;
     cursor: pointer;
-    width: 40px;
-    height: 40px;
-    img {
-      width: 100%;
-      height: 100%;
-      border-radius: 50%;
-    }
+   }
+   > .link {
+    text-decoration: none;
+    border-radius: 2px;
+    /* background-color: #f5feff; */
+    padding: 2px;
+    color: #1fe1f9;
+   }
   }
-  > .details {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-
-    .up {
-      display: flex;
-      align-items: center;
-      gap: 8px;
-      font-weight: 500;
-      .name {
-        color: #06b6d4;
-        font-style: normal;
-        font-size: 14px;
-        line-height: 20px;
-      }
-      .time {
-        color: #bfbfbf;
-        font-size: 12px;
-        line-height: 18px;
-      }
-    }
-    .down {
-      user-select: text;
-      color: #374151;
-      font-weight: normal;
-      font-size: 14px;
-      line-height: 20px;
-      word-break: break-all;
-      white-space: break-spaces;
-      .edited {
-        margin-left: 5px;
-        color: #999;
-        font-size: 10px;
-      }
-      &.sending {
-        opacity: 0.9;
-      }
-      .img {
-        max-width: 480px;
-        cursor: pointer;
-      }
-      > .link {
-        text-decoration: none;
-        border-radius: 2px;
-        /* background-color: #f5feff; */
-        padding: 2px;
-        color: #1fe1f9;
-      }
-    }
-  }
+ }
 `;
 
 export default StyledMsg;
--- src/routes/chat/ChannelList/NavItem.js
+++ src/routes/chat/ChannelList/NavItem.js
@@ -1,199 +1,185 @@
-import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useDrop } from "react-dnd";
-import { NativeTypes } from "react-dnd-html5-backend";
-import { useSelector } from "react-redux";
-import Tippy from "@tippyjs/react";
-import useContextMenu from "../../../common/hook/useContextMenu";
-import ContextMenu from "../../../common/component/ContextMenu";
-import InviteModal from "../../../common/component/ChannelInviteModal";
-import Tooltip from "../../../common/component/Tooltip";
-import IconSetting from "../../../assets/icons/setting.svg";
-import IconInvite from "../../../assets/icons/invite.from.channel.svg";
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useDrop } from 'react-dnd';
+import { NativeTypes } from 'react-dnd-html5-backend';
+import { useSelector } from 'react-redux';
+import Tippy from '@tippyjs/react';
+import useContextMenu from '../../../common/hook/useContextMenu';
+import ContextMenu from '../../../common/component/ContextMenu';
+import InviteModal from '../../../common/component/ChannelInviteModal';
+import Tooltip from '../../../common/component/Tooltip';
+import IconSetting from '../../../assets/icons/setting.svg';
+import IconInvite from '../../../assets/icons/invite.from.channel.svg';
 // import { useDebounce} from "rooks";
-import { useReadMessageMutation } from "../../../app/services/message";
-import { useUpdateMuteSettingMutation } from "../../../app/services/contact";
+import { useReadMessageMutation } from '../../../app/services/message';
+import { useUpdateMuteSettingMutation } from '../../../app/services/contact';
 
-import StyledLink from "./styled";
-import ChannelIcon from "../../../common/component/ChannelIcon";
-import { getUnreadCount } from "../utils";
+import StyledLink from './styled';
+import ChannelIcon from '../../../common/component/ChannelIcon';
+import { getUnreadCount } from '../utils';
 
 const NavItem = ({ id, setFiles, toggleRemoveConfirm }) => {
-  const { pathname } = useLocation();
-  const [inviteModalVisible, setInviteModalVisible] = useState(false);
-  const navigate = useNavigate();
-  const [muteChannel] = useUpdateMuteSettingMutation();
-  const [updateReadIndex] = useReadMessageMutation();
+ const { pathname } = useLocation();
+ const [inviteModalVisible, setInviteModalVisible] = useState(false);
+ const navigate = useNavigate();
+ const [muteChannel] = useUpdateMuteSettingMutation();
+ const [updateReadIndex] = useReadMessageMutation();
 
-  const {
-    visible: contextMenuVisible,
-    offset,
-    handleContextMenuEvent,
-    hideContextMenu,
-  } = useContextMenu();
-  const {
-    channel,
-    mids,
-    messageData,
-    readIndex,
-    muted,
-    loginUid,
-    loginUser,
-  } = useSelector((store) => {
-    return {
-      loginUser: store.contacts.byId[store.authData.uid],
-      channel: store.channels.byId[id],
-      mids: store.channelMessage[id],
-      messageData: store.message,
-      loginUid: store.authData.uid,
-      readIndex: store.footprint.readChannels[id],
-      muted: store.footprint.muteChannels[id],
-    };
-  });
-  const handleChannelSetting = (evt) => {
-    evt.preventDefault();
-    evt.stopPropagation();
-    const { id } = evt.target.dataset;
-    if (id) {
-      navigate(`/setting/channel/${id}?f=${pathname}`);
+ const {
+  visible: contextMenuVisible,
+  offset,
+  handleContextMenuEvent,
+  hideContextMenu
+ } = useContextMenu();
+ const { channel, mids, messageData, readIndex, muted, loginUid, loginUser } = useSelector(
+  (store) => {
+   return {
+    loginUser: store.contacts.byId[store.authData.uid],
+    channel: store.channels.byId[id],
+    mids: store.channelMessage[id],
+    messageData: store.message,
+    loginUid: store.authData.uid,
+    readIndex: store.footprint.readChannels[id],
+    muted: store.footprint.muteChannels[id]
+   };
+  }
+ );
+ const handleChannelSetting = (evt) => {
+  evt.preventDefault();
+  evt.stopPropagation();
+  const { id } = evt.currentTarget.dataset;
+  if (id) {
+   navigate(`/setting/channel/${id}?f=${pathname}`);
+  }
+ };
+ const [{ isActive }, drop] = useDrop(() => ({
+  accept: [NativeTypes.FILE],
+  drop({ dataTransfer }) {
+   if (dataTransfer.files.length) {
+    // console.log(files, rest);
+    setFiles([...dataTransfer.files]);
+    navigate(`/chat/channel/${id}`);
+    // 重置
+    setTimeout(() => {
+     setFiles([]);
+    }, 300);
+   }
+  },
+  collect: (monitor) => ({
+   isActive: monitor.canDrop() && monitor.isOver()
+  })
+ }));
+ const handleReadAll = () => {
+  const lastMid = mids[mids.length - 1];
+  console.log('last mid', mids, lastMid);
+  if (lastMid) {
+   const param = { groups: [{ gid: id, mid: lastMid }] };
+   updateReadIndex(param);
+  }
+ };
+ const toggleInviteModalVisible = (evt) => {
+  if (evt) {
+   evt.preventDefault();
+   evt.stopPropagation();
+  }
+  setInviteModalVisible((prev) => !prev);
+ };
+ const handleMute = () => {
+  const data = muted ? { remove_groups: [id] } : { add_groups: [{ gid: id }] };
+  muteChannel(data);
+ };
+ const { is_public, name, owner } = channel;
+ const { unreads = 0, mentions = [] } = getUnreadCount({
+  mids,
+  messageData,
+  readIndex,
+  loginUid
+ });
+ const isMentions = mentions.length !== 0;
+ const inviteIconVisible = loginUser?.is_admin || owner == loginUid;
+ return (
+  <>
+   <Tippy
+    interactive
+    placement="right-start"
+    popperOptions={{ strategy: 'fixed' }}
+    offset={[offset.y, offset.x]}
+    visible={contextMenuVisible}
+    onClickOutside={hideContextMenu}
+    key={id}
+    content={
+     <ContextMenu
+      hideMenu={hideContextMenu}
+      items={[
+       {
+        title: 'Mark As Read',
+        underline: true,
+        handler: handleReadAll
+       },
+       {
+        title: muted ? 'Unmute' : 'Mute',
+        handler: handleMute
+       },
+       {
+        title: 'Notification Settings',
+        underline: true
+       },
+       is_public
+        ? null
+        : {
+           title: 'Invite People'
+          },
+       {
+        title: 'Delete Channel',
+        danger: true,
+        handler: toggleRemoveConfirm.bind(null, id)
+       }
+      ]}
+     />
     }
-  };
-  const [{ isActive }, drop] = useDrop(() => ({
-    accept: [NativeTypes.FILE],
-    drop({ dataTransfer }) {
-      if (dataTransfer.files.length) {
-        // console.log(files, rest);
-        setFiles([...dataTransfer.files]);
-        navigate(`/chat/channel/${id}`);
-        // 重置
-        setTimeout(() => {
-          setFiles([]);
-        }, 300);
-      }
-    },
-    collect: (monitor) => ({
-      isActive: monitor.canDrop() && monitor.isOver(),
-    }),
-  }));
-  const handleReadAll = () => {
-    const lastMid = mids[mids.length - 1];
-    console.log("last mid", mids, lastMid);
-    if (lastMid) {
-      const param = { groups: [{ gid: id, mid: lastMid }] };
-      updateReadIndex(param);
-    }
-  };
-  const toggleInviteModalVisible = (evt) => {
-    if (evt) {
-      evt.preventDefault();
-      evt.stopPropagation();
-    }
-    setInviteModalVisible((prev) => !prev);
-  };
-  const handleMute = () => {
-    const data = muted
-      ? { remove_groups: [id] }
-      : { add_groups: [{ gid: id }] };
-    muteChannel(data);
-  };
-  const { is_public, name, owner } = channel;
-  const { unreads = 0, mentions = [] } = getUnreadCount({
-    mids,
-    messageData,
-    readIndex,
-    loginUid,
-  });
-  const isMentions = mentions.length !== 0;
-  const inviteIconVisible = loginUser?.is_admin || owner == loginUid;
-  return (
-    <>
-      <Tippy
-        interactive
-        placement="right-start"
-        popperOptions={{ strategy: "fixed" }}
-        offset={[offset.y, offset.x]}
-        visible={contextMenuVisible}
-        onClickOutside={hideContextMenu}
-        key={id}
-        content={
-          <ContextMenu
-            hideMenu={hideContextMenu}
-            items={[
-              {
-                title: "Mark As Read",
-                underline: true,
-                handler: handleReadAll,
-              },
-              {
-                title: muted ? "Unmute" : "Mute",
-                handler: handleMute,
-              },
-              {
-                title: "Notification Settings",
-                underline: true,
-              },
-              is_public
-                ? null
-                : {
-                    title: "Invite People",
-                  },
-              {
-                title: "Delete Channel",
-                danger: true,
-                handler: toggleRemoveConfirm.bind(null, id),
-              },
-            ]}
-          />
-        }
-      >
-        <StyledLink
-          data-cid={id}
-          onContextMenu={handleContextMenuEvent}
-          ref={drop}
-          key={id}
-          className={`link ${isActive ? "drop_over" : ""} ${
-            muted ? "muted" : ""
-          }`}
-          to={`/chat/channel/${id}`}
-        >
-          <div className={`name`} title={name}>
-            <ChannelIcon personal={!is_public} muted={muted} />
-            <span className={`txt ${unreads == 0 ? "read" : ""}`}>{name}</span>
-          </div>
-          <div className="icons">
-            {inviteIconVisible && (
-              <Tooltip placement="bottom" tip="Add Member">
-                <IconInvite
-                  className="icon invite"
-                  data-id={id}
-                  onClick={toggleInviteModalVisible}
-                ></IconInvite>
-              </Tooltip>
-            )}
-            <Tooltip placement="bottom" tip="Channel Setting">
-              <IconSetting
-                className="icon setting"
-                data-id={id}
-                onClick={handleChannelSetting}
-              ></IconSetting>
-            </Tooltip>
-            {unreads > 0 && (
-              <i className={`badge ${isMentions ? "mention" : ""}`}>
-                {isMentions ? mentions.length : unreads}
-              </i>
-            )}
-          </div>
-        </StyledLink>
-      </Tippy>
-      {inviteModalVisible && (
-        <InviteModal
-          cid={id}
-          title={channel.name}
-          closeModal={toggleInviteModalVisible}
-        />
+   >
+    <StyledLink
+     data-cid={id}
+     onContextMenu={handleContextMenuEvent}
+     ref={drop}
+     key={id}
+     className={`link ${isActive ? 'drop_over' : ''} ${muted ? 'muted' : ''}`}
+     to={`/chat/channel/${id}`}
+    >
+     <div className={`name`} title={name}>
+      <ChannelIcon personal={!is_public} muted={muted} />
+      <span className={`txt ${unreads == 0 ? 'read' : ''}`}>{name}</span>
+     </div>
+     <div className="icons">
+      {inviteIconVisible && (
+       <Tooltip placement="bottom" tip="Add Member">
+        <IconInvite
+         className="icon invite"
+         data-id={id}
+         onClick={toggleInviteModalVisible}
+        ></IconInvite>
+       </Tooltip>
       )}
-    </>
-  );
+      <Tooltip placement="bottom" tip="Channel Setting">
+       <IconSetting
+        className="icon setting"
+        data-id={id}
+        onClick={handleChannelSetting}
+       ></IconSetting>
+      </Tooltip>
+      {unreads > 0 && (
+       <i className={`badge ${isMentions ? 'mention' : ''}`}>
+        {isMentions ? mentions.length : unreads}
+       </i>
+      )}
+     </div>
+    </StyledLink>
+   </Tippy>
+   {inviteModalVisible && (
+    <InviteModal cid={id} title={channel.name} closeModal={toggleInviteModalVisible} />
+   )}
+  </>
+ );
 };
 
 export default NavItem;
