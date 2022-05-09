f3b82295d634c495b544ff9219248980c5ca4f0f
zerosoul <yanggc888@163.com>
2022-05-09 10:21:12 +0800

    chore: faq

--- src/common/component/FAQ.js
+++ src/common/component/FAQ.js
@@ -1,5 +1,20 @@
 // import React from "react";
+import styled from "styled-components";
+import pkg from "../../../package.json";
+
+const Styled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
 export default function FAQ() {
   console.log("build time", process.env);
-  return <div>Build Timestamp: {process.env.REACT_APP_BUILD_TIME}</div>;
+  return (
+    <Styled>
+      <div className="item">
+        Build Timestamp: {process.env.REACT_APP_BUILD_TIME}
+      </div>
+      <div className="item">Version: {pkg.version}</div>
+    </Styled>
+  );
 }
