33db3364f537f6f7ef1fa7a6dec67cd34e18e2e7
HuanCheng Bai <bestony@linux.com>
2022-05-10 21:22:41 +0800

    feat: add owner style

--- src/common/component/VideoChat/Owner.js
+++ src/common/component/VideoChat/Owner.js
@@ -1,10 +1,34 @@
-import {
-    AgoraVideoPlayer
-} from "agora-rtc-react";
+import { AgoraVideoPlayer } from "agora-rtc-react";
+import styled from "styled-components";
+const Cell = styled.div`
+  display: block;
+  width: 100%;
+  .video {
+    width: 226px;
+    padding: 10px;
+    overflow: hidden;
+    border-radius: 20px;
+  }
+  .nameTag {
+    display: inline-block;
+    position: relative;
+    z-index: 90;
+    background-color: #000;
+    color: #fff;
+    padding: 3px 5px;
+    top: -22px;
+  }
+`;
 export default function Owner({ track }) {
-    return (<div>
-        <div className="video">
-            <AgoraVideoPlayer videoTrack={track} style={{ height: '180px', width: '100%' }} />
-        </div>
-    </div>);
+  return (
+    <Cell>
+      <div className="video">
+        <AgoraVideoPlayer
+          videoTrack={track}
+          style={{ height: "180px", width: "100%" }}
+        />
+        <div className="nameTag">当前用户</div>
+      </div>
+    </Cell>
+  );
 }
