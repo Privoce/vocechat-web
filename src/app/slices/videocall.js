34022d5dd52bfd96525510361f8d3206c3403ded
bestony <bestony@linux.com>
2022-05-19 20:46:45 +0800

    feat: add params for rtm

--- src/app/slices/videocall.js
+++ src/app/slices/videocall.js
@@ -1,14 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
- replying: {}
+const initialState = {};
+
+// 定义 AGORA 信令
+// 格式：user,cmd
+// Example: 1,LOGIN
+
+const commandMap = {
+ login: "LOGIN",
+ join: "JOIN",
+ openMic: "OPENMIC",
+ closeMic: "CLOSEMIC",
+ openVideo: "OPENVIDEO",
+ closeVideo: "CLOSEVIDEO",
+ leave: "LEAVE"
 };
+
+// function parseCMD(command) {
+//   var cmds = command.split(",");
+//   return {
+//     user: cmds[0],
+//     cmd: cmds[1],
+//     args: JSON.parse(cmds[2]),
+//   };
+// }
+
+function buildCMD(user, cmd, args) {
+ const argString = JSON.stringify(args);
+ return `${user},${cmd.toUpperCase()},${argString}`;
+}
 
 const videocallSlice = createSlice({
  name: "videocall",
  initialState,
  reducers: {
-  resetVideocall() {}
+  resetVideocall() {},
+  joinChannel(name) {
+   window.clientInfo.join(name);
+   window.rtmClient.send(buildCMD(1, commandMap.login));
+  }
  }
 });
 
