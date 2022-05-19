7797f6c4f54ea4eae9437ad9e78e2c745846e8cf
bestony <bestony@linux.com>
2022-05-19 22:26:15 +0800

    feat: add user component

--- src/common/component/VideoCall/User.js
+++ src/common/component/VideoCall/User.js
@@ -2,26 +2,20 @@ import styled from "styled-components";
 import Avatar from "../Avatar";
 import MicOnIcon from "../../../assets/icons/mic.on.svg?url";
 import MicDisabledIcon from "../../../assets/icons/mic.disabled.svg?url";
-import soundOnIcon from "../../assets/icons/sound.on.svg?url";
-import soundOffIcon from "../../assets/icons/sound.off.svg?url";
+import soundOnIcon from "../../../assets/icons/sound.on.svg?url";
+import soundOffIcon from "../../../assets/icons/sound.off.svg?url";
 const Wrapper = styled.div`
  display: flex;
- flex-direction: column;
+ flex-direction: row;
  align-items: center;
  padding: 8px;
  gap: 2px;
 
- width: 226px;
+ width: 210px;
  height: 64px;
 
- /* Base/White */
-
  background: #ffffff;
- /* top divider */
-
  box-shadow: 0px -1px 0px rgba(0, 0, 0, 0.05);
-
- /* Inside auto layout */
 
  flex: none;
  order: 1;
@@ -29,9 +23,18 @@ const Wrapper = styled.div`
  .people {
   width: 130px;
   height: 32px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
   flex: none;
   order: 0;
   flex-grow: 0;
+  .avatar {
+   width: 32px;
+   height: 32px;
+   border-radius: 16px;
+  }
   .name {
    font-family: "Inter";
    font-style: normal;
@@ -47,25 +50,27 @@ const Wrapper = styled.div`
   align-items: flex-start;
   padding: 0px;
   gap: 8px;
-
   width: 40px;
   height: 16px;
-
   flex: none;
   order: 1;
   flex-grow: 0;
+  .icon {
+   width: 16px;
+   height: 16px;
+  }
  }
 `;
 export default function user({ id, openMic, openVideo }) {
  return (
   <Wrapper>
    <div className="people">
-    <Avatar name={id} />
+    <Avatar className="avatar" name={id} />
     <div className="name">丁一</div>
    </div>
    <div className="action">
-    <img src={openMic ? MicOnIcon : MicDisabledIcon} alt="mic control" />
-    <img src={openVideo ? soundOnIcon : soundOffIcon} alt="mic control" />
+    <img src={openMic ? MicOnIcon : MicDisabledIcon} className="icon" alt="mic control" />
+    <img src={openVideo ? soundOnIcon : soundOffIcon} className="icon" alt="mic control" />
    </div>
   </Wrapper>
  );
