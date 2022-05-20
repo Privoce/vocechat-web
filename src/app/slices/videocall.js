19e1152bb8959e7b36b6bea8f3fabecfe13197b0
bestony <bestony@linux.com>
2022-05-20 22:46:27 +0800

    feat: add some new redux state

--- src/app/slices/videocall.js
+++ src/app/slices/videocall.js
@@ -2,13 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
  openPanel: false,
- users: [], // user struct {id,name,openVideo,openMic}
- devices: {
+ users: [
+  { id: 1, username: "123", openVideo: true, openMic: false },
+  { id: 2, username: "123", openVideo: false, openMic: true },
+  { id: 4, username: "123", openVideo: false, openMic: true }
+ ], // user struct {id,name,openVideo,openMic}
+ // 用于控制本地的设备
+ device: {
+  mic: false,
+  camera: false,
+  share: false,
   currentMic: "default",
-  currentCamera: "default",
-  mics: [],
-  cameras: [],
-  playbacks: []
+  currentCamera: "default"
+ },
+ // 用于记录所有系统设备
+ devices: {
+  mics: [{ id: "default", label: "Default" }],
+  cameras: [{ id: "default", label: "Default" }],
+  playbacks: [{ id: "default", label: "Default" }]
  }
 };
 
@@ -44,13 +55,27 @@ const videocallSlice = createSlice({
  name: "videocall",
  initialState,
  reducers: {
-  resetVideocall() {},
-  joinChannel(name) {
+  toggleMic: (state) => {
+   state.devices.mic = !state.devices.mic;
+  },
+  resetState: (state) => {
+   state.users = [];
+  },
+  join: (state, payload) => {
+   // add current channel name;
+   state.channelName = payload;
+   // join rtc channel
+   // join rtm channel
+  },
+  test_1(name) {
    window.clientInfo.join(name);
    window.rtmClient.send(buildCMD(1, commandMap.login));
   }
  }
 });
 
-export const { resetVideocall } = videocallSlice.actions;
+export const selectDevice = (state) => state.videocall.devices;
+export const selectUsers = (state) => state.videocall.users;
+
+export const { resetState } = videocallSlice.actions;
 export default videocallSlice.reducer;
