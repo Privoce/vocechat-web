ee8b463540c1df7460b19e59615c45c694f80c07
zerosoul <yanggc888@163.com>
2022-06-05 13:15:25 +0800

    build: upgrade packages

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,6 @@
 // const BASE_URL = `${location.origin}/api`;
 const BASE_URL = `https://dev.rustchat.com/api`;
-export const CACHE_VERSION = `0.2.16`;
+export const CACHE_VERSION = `0.2.17`;
 export const ContentTypes = {
   text: "text/plain",
   markdown: "text/markdown",
