b63cb062993d2c2942277193827a86ddc69c6c1a
Tristan Yang <yanggc888@163.com>
2022-10-28 09:00:42 +0800

    Updates

---
+++ static/js/725.fb596ed7.chunk.js
@@ -0,0 +1,97 @@
+"use strict";(globalThis.webpackChunkvocechat_web=globalThis.webpackChunkvocechat_web||[]).push([[725],{3916:(e,t,n)=>{n.d(t,{Z:()=>c});var i,o,r=n(537);function s(){return s=Object.assign?Object.assign.bind():function(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var i in n)Object.prototype.hasOwnProperty.call(n,i)&&(e[i]=n[i])}return e},s.apply(this,arguments)}const a=(e,t)=>{let{title:n,titleId:a,...c}=e;return r.createElement("svg",s({width:25,height:24,viewBox:"0 0 25 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",ref:t,"aria-labelledby":a},c),n?r.createElement("title",{id:a},n):null,i||(i=r.createElement("g",{clipPath:"url(#clip0_14990_39524)"},r.createElement("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M12.5 0C5.8724 0 0.5 5.3808 0.5 12.0204C0.5 17.3304 3.938 21.8364 8.7068 23.4252C9.3068 23.5356 9.5252 23.1648 9.5252 22.8456C9.5252 22.5612 9.5156 21.804 9.5096 20.802C6.1712 21.528 5.4668 19.1904 5.4668 19.1904C4.922 17.8008 4.1348 17.4312 4.1348 17.4312C3.0452 16.6872 4.2176 16.7016 4.2176 16.7016C5.4212 16.7856 6.0548 17.94 6.0548 17.94C7.1252 19.776 8.864 19.2456 9.5468 18.9384C9.6572 18.162 9.9668 17.6328 10.31 17.3328C7.646 17.0292 4.844 15.9972 4.844 11.3916C4.844 10.08 5.312 9.006 6.0788 8.166C5.9552 7.8624 5.5436 6.6396 6.1964 4.986C6.1964 4.986 7.2044 4.662 9.4964 6.2172C10.4753 5.95022 11.4853 5.81423 12.5 5.8128C13.52 5.8176 14.546 5.9508 15.5048 6.2172C17.7956 4.662 18.8012 4.9848 18.8012 4.9848C19.4564 6.6396 19.0436 7.8624 18.9212 8.166C19.6892 9.006 20.1548 10.08 20.1548 11.3916C20.1548 16.0092 17.348 17.0256 14.6756 17.3232C15.1064 17.694 15.4892 18.4272 15.4892 19.5492C15.4892 21.1548 15.4748 22.452 15.4748 22.8456C15.4748 23.1672 15.6908 23.5416 16.3004 23.424C18.69 22.6225 20.7672 21.0904 22.2386 19.0441C23.7099 16.9977 24.501 14.5408 24.5 12.0204C24.5 5.3808 19.1264 0 12.5 0Z",fill:"black"}))),o||(o=r.createElement("defs",null,r.createElement("clipPath",{id:"clip0_14990_39524"},r.createElement("rect",{width:24,height:24,fill:"white",transform:"translate(0.5)"})))))},c=(0,r.forwardRef)(a)},6049:(e,t,n)=>{n.d(t,{Z:()=>d});var i,o,r,s,a=n(537);function c(){return c=Object.assign?Object.assign.bind():function(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var i in n)Object.prototype.hasOwnProperty.call(n,i)&&(e[i]=n[i])}return e},c.apply(this,arguments)}const l=(e,t)=>{let{title:n,titleId:l,...d}=e;return a.createElement("svg",c({width:2443,height:2500,viewBox:"0 0 256 262",xmlns:"http://www.w3.org/2000/svg",preserveAspectRatio:"xMidYMid",ref:t,"aria-labelledby":l},d),n?a.createElement("title",{id:l},n):null,i||(i=a.createElement("path",{d:"M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027",fill:"#4285F4"})),o||(o=a.createElement("path",{d:"M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1",fill:"#34A853"})),r||(r=a.createElement("path",{d:"M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782",fill:"#FBBC05"})),s||(s=a.createElement("path",{d:"M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251",fill:"#EB4335"})))},d=(0,a.forwardRef)(l)},9879:(e,t,n)=>{n.d(t,{Z:()=>g});var i=n(537),o=n(3916),r=n(7889),s=n(308),a=n(9885),c=n(5312),l=n(7418),d=n(683);const u=(0,r.ZP)(a.Z)`
+  width: 100%;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  color: #344054;
+  border: 1px solid #d0d5dd;
+  background: none !important;
+  .icon {
+    width: 24px;
+    height: 24px;
+  }
+`,g=e=>{let{type:t="login",client_id:n}=e;const r=localStorage.getItem(s.LJ),[a,{isLoading:g,isSuccess:h,error:p}]=(0,c.YA)();(0,i.useEffect)((()=>{const e=new URLSearchParams(location.search),t="github"===e.get("oauth"),n=e.get("code");t&&n&&a({magic_token:r,code:n,type:"github"})}),[]),(0,i.useEffect)((()=>{h&&l.ZP.success("Login Successfully")}),[h]),(0,i.useEffect)((()=>{if(p)if(410===p.status)l.ZP.error("No associated account found, please user admin for an invitation link to join.");else l.ZP.error("Something Error");else;}),[p]);return(0,d.jsxs)(u,{onClick:()=>{location.href=`https://github.com/login/oauth/authorize?client_id=${n}`},disabled:g,children:[(0,d.jsx)(o.Z,{className:"icon"}),` ${"login"===t?"Sign in":"Sign up"} with Github`]})}},3674:(e,t,n)=>{n.d(t,{Z:()=>p});var i=n(537),o=n(9156),r=n(7418),s=n(7889),a=n(308),c=n(6049),l=n(9885),d=n(5312),u=n(683);const g=(0,s.ZP)(l.Z)`
+  position: relative;
+  width: 100%;
+  margin-bottom: 16px;
+  background: white !important;
+  border: 1px solid #d0d5dd;
+  color: #344054;
+  height: 42px;
+  overflow: hidden;
+  .mask {
+    background: inherit;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+    z-index: 998;
+    height: 40px;
+    .icon {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  > .hide {
+    /* z-index: 1; */
+    /* opacity: 0; */
+    left: 0;
+    top: 0;
+    position: absolute;
+    width: 100%;
+    iframe {
+      width: 100% !important;
+    }
+  }
+  &:hover {
+     .hide {
+      opacity: 0;
+      z-index: 999;
+    }
+  }
+`,h=e=>{let{type:t="login",loaded:n,loadError:s}=e;const[l,{isSuccess:h,isLoading:p,error:f}]=(0,d.YA)(),m=localStorage.getItem(a.LJ);return(0,i.useEffect)((()=>{h&&r.ZP.success("Login Successfully")}),[h]),(0,i.useEffect)((()=>{if(f&&"status"in f)if(410===f.status)r.ZP.error("No associated account found, please user admin for an invitation link to join.");else r.ZP.error("Something Error");else;}),[f]),(0,u.jsxs)(g,{disabled:!n||p,children:[(0,u.jsxs)("div",{className:"mask",children:[(0,u.jsx)(c.Z,{className:"icon"}),s?"Script Load Error!":n?("login"===t?"Sign in":"Sign up")+" with Google":"Initializing"]}),(0,u.jsx)("div",{className:"hide",children:(0,u.jsx)(o.kZ,{width:"360px",onSuccess:e=>{l({magic_token:m,id_token:e.credential||"",type:"google"})}})})]})},p=e=>{let{type:t="login",clientId:n}=e;const[r,s]=(0,i.useState)(!1),[a,c]=(0,i.useState)(!1);return n?(0,u.jsx)(o.rg,{onScriptLoadError:()=>{c(!0)},onScriptLoadSuccess:()=>{s(!0)},clientId:n,children:(0,u.jsx)(h,{type:t,loaded:r,loadError:a})}):null}},2712:(e,t,n)=>{n.d(t,{Z:()=>r});var i=n(537),o=n(5809);function r(){const[e,t]=(0,i.useState)(!1),[n,r]=(0,i.useState)(),{data:s}=(0,o.R)(void 0,{refetchOnMountOrArgChange:!0}),[a,{isSuccess:c}]=(0,o.Ku)();(0,i.useEffect)((()=>{s&&r(s)}),[s]),(0,i.useEffect)((()=>{t(!c&&JSON.stringify(s)!==JSON.stringify(n))}),[s,n,c]);return{config:n,changed:e,updateGithubAuthConfig:e=>{r((t=>t?{...t,...e}:e))},updateGithubAuthConfigToServer:async()=>{n&&await a(n)},isSuccess:c}}},5089:(e,t,n)=>{n.d(t,{Z:()=>r});var i=n(537),o=n(5809);function r(){const[e,t]=(0,i.useState)(!1),[n,r]=(0,i.useState)(""),{data:s}=(0,o.eM)(void 0,{refetchOnMountOrArgChange:!0}),[a,{isSuccess:c}]=(0,o.Qg)();(0,i.useEffect)((()=>{s&&r(s.client_id)}),[s]),(0,i.useEffect)((()=>{t(!c&&(null===s||void 0===s?void 0:s.client_id)!==n)}),[s,n,c]);return{config:s,changed:e,clientId:n,updateClientId:r,updateClientIdToServer:async()=>{n&&await a({client_id:n})},updateGoogleAuthConfig:a,isSuccess:c}}},2725:(e,t,n)=>{n.r(t),n.d(t,{default:()=>S});var i=n(537),o=n(7418),r=n(308),s=n(3022),a=n(9885),c=n(5312),l=n(7889),d=n(683);const u=l.ZP.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .title {
+    font-weight: 600;
+    font-size: 30px;
+    line-height: 38px;
+    color: #101828;
+    margin-bottom: 12px;
+  }
+  .desc {
+    text-align: center;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: #667085;
+    &:not(:last-child) {
+      margin-bottom: 24px;
+    }
+  }
+`;function g(){return(0,d.jsxs)(u,{children:[(0,d.jsx)("div",{className:"title",children:"Magic link Sent"}),(0,d.jsx)("p",{className:"desc",children:"Login to your email client, and continue next step"}),(0,d.jsx)("p",{className:"desc",children:"You can close this window now."})]})}const h=l.ZP.p`
+  text-align: center;
+  margin: 24px 0 8px;
+  > span {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    color: #667085;
+    margin-right: 4px;
+  }
+
+  > a {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+    color: #22d3ee;
+    cursor: pointer;
+  }
+`;function p(){return(0,d.jsxs)(h,{children:[(0,d.jsx)("span",{children:"Have an account?"}),(0,d.jsx)("a",{onClick:()=>{location.href="/#/login"},children:"Sign In"})]})}var f=n(5809),m=n(2712),w=n(5089),x=n(3674),v=n(9879);function S(){const[e,{isLoading:t,data:n,isSuccess:l}]=(0,c.uk)(),[u,{isLoading:h}]=(0,c.N0)(),[S,C]=(0,i.useState)(""),[y,b]=(0,i.useState)({email:"",password:"",confirmPassword:""});(0,i.useEffect)((()=>{const e=new URLSearchParams(location.search).get("magic_token");e&&(localStorage.setItem(r.LJ,e),C(e))}),[]),(0,i.useEffect)((()=>{if(l&&n){const{new_magic_token:e,mail_is_sent:t}=n;!t&&e&&(location.href=`?magic_token=${e}#/register/set_name`)}}),[l,n]);const E=e=>{const{type:t}=e.target.dataset,{value:n}=e.target;b((e=>(e[t]=n,{...e})))},{clientId:j}=(0,w.Z)(),{config:Z}=(0,m.Z)(),{data:P,isSuccess:_}=(0,f.ww)();if(!_)return null;const{github:k,google:L,who_can_sign_up:N}=P,I=L&&j;if("EveryOne"!==N&&!S)return(0,d.jsx)(d.Fragment,{children:"Sign up method is updated to Invitation Link Only"});const{email:O,password:R,confirmPassword:z}=y;if(null!==n&&void 0!==n&&n.mail_is_sent)return(0,d.jsx)(g,{});const M=t||h;return(0,d.jsxs)(d.Fragment,{children:[(0,d.jsxs)("div",{className:"tips",children:[(0,d.jsx)("img",{src:`${r.ZP}/resource/organization/logo`,alt:"logo",className:"logo"}),(0,d.jsx)("h2",{className:"title",children:"Sign Up to VoceChat"}),(0,d.jsx)("span",{className:"desc",children:"Please enter your details."})]}),(0,d.jsxs)("form",{onSubmit:async t=>{t.preventDefault();const{email:n,password:i,confirmPassword:r}=y;if(i!==r)return void o.ZP.error("Not Same Password!");const{data:s}=await u(n);s?e({magic_token:S,email:n,password:i}):o.ZP.error("Email already registered!")},autoSave:"false",autoComplete:"true",children:[(0,d.jsx)(s.Z,{className:"large",name:"email",value:O,required:!0,placeholder:"Enter email","data-type":"email",onChange:E}),(0,d.jsx)(s.Z,{className:"large",type:"password",value:R,name:"password",required:!0,"data-type":"password",onChange:E,placeholder:"Enter password"}),(0,d.jsx)(s.Z,{required:!0,onBlur:()=>{const{password:e,confirmPassword:t}=y;e!==t&&o.ZP.error("Not Same Password!")},type:"password",name:"confirmPassword",value:z,"data-type":"confirmPassword",onChange:E,placeholder:"Confirm Password"}),(0,d.jsx)(a.Z,{type:"submit",disabled:M,children:M?"Signing Up":"Sign Up"})]}),(0,d.jsx)("hr",{className:"or"}),I&&(0,d.jsx)(x.Z,{type:"register",clientId:j}),k&&(0,d.jsx)(v.Z,{type:"register",client_id:null===Z||void 0===Z?void 0:Z.client_id}),(0,d.jsx)(p,{})]})}},9156:(e,t,n)=>{n.d(t,{kZ:()=>c,rg:()=>r});var i=n(537);const o=(0,i.createContext)(null);function r(e){let{clientId:t,onScriptLoadSuccess:n,onScriptLoadError:r,children:s}=e;const a=function(){let e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:{};const{onScriptLoadSuccess:t,onScriptLoadError:n}=e,[o,r]=(0,i.useState)(!1),s=(0,i.useRef)(t);s.current=t;const a=(0,i.useRef)(n);return a.current=n,(0,i.useEffect)((()=>{const e=document.createElement("script");return e.src="https://accounts.google.com/gsi/client",e.async=!0,e.defer=!0,e.onload=()=>{var e;r(!0),null===(e=s.current)||void 0===e||e.call(s)},e.onerror=()=>{var e;r(!1),null===(e=a.current)||void 0===e||e.call(a)},document.body.appendChild(e),()=>{document.body.removeChild(e)}}),[]),o}({onScriptLoadSuccess:n,onScriptLoadError:r}),c=(0,i.useMemo)((()=>({clientId:t,scriptLoadedSuccessfully:a})),[t,a]);return i.createElement(o.Provider,{value:c},s)}function s(){const e=(0,i.useContext)(o);if(!e)throw new Error("Google OAuth components must be used within GoogleOAuthProvider");return e}const a={large:40,medium:32,small:20};function c(e){let{onSuccess:t,onError:n,useOneTap:o,promptMomentNotification:r,type:c="standard",theme:l="outline",size:d="large",text:u,shape:g,logo_alignment:h,width:p,locale:f,...m}=e;const w=(0,i.useRef)(null),{clientId:x,scriptLoadedSuccessfully:v}=s(),S=(0,i.useRef)(t);S.current=t;const C=(0,i.useRef)(n);C.current=n;const y=(0,i.useRef)(r);return y.current=r,(0,i.useEffect)((()=>{var e,t,n;if(v)return null===(e=window.google)||void 0===e||e.accounts.id.initialize({client_id:x,callback:e=>{var t;if(!e.clientId||!e.credential)return null===(t=C.current)||void 0===t?void 0:t.call(C);S.current(e)},...m}),null===(t=window.google)||void 0===t||t.accounts.id.renderButton(w.current,{type:c,theme:l,size:d,text:u,shape:g,logo_alignment:h,width:p,locale:f}),o&&(null===(n=window.google)||void 0===n||n.accounts.id.prompt(y.current)),()=>{var e;o&&(null===(e=window.google)||void 0===e||e.accounts.id.cancel())}}),[x,v,o,c,l,d,u,g,h,p,f]),i.createElement("div",{ref:w,style:{height:a[d]}})}}}]);
---
+++ widget.js
@@ -0,0 +1,40 @@
+const {
+  domain = location.origin,
+  hostId = 1,
+  closeWidth = 52,
+  closeHeight = 52,
+  openWidth = 600,
+  openHeight = 800
+} = document.currentScript.dataset;
+const wrapper = document.createElement("iframe");
+const styles = {
+  position: "fixed",
+  right: "15px",
+  bottom: "15px",
+  border: "none"
+};
+Object.assign(wrapper.style, styles);
+wrapper.src = `${domain}/widget.html?host=${hostId}`;
+wrapper.width = closeWidth;
+wrapper.height = closeHeight;
+wrapper.frameborder = 0;
+window.addEventListener(
+  "message",
+  (event) => {
+    const { data: CMD } = event;
+    switch (CMD) {
+      case "OPEN":
+        wrapper.setAttribute("width", openWidth);
+        wrapper.setAttribute("height", openHeight);
+        break;
+      case "CLOSE":
+        wrapper.setAttribute("width", closeWidth);
+        wrapper.setAttribute("height", closeHeight);
+        break;
+      default:
+        break;
+    }
+  },
+  false
+);
+document.body.appendChild(wrapper);
