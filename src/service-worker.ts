9b3dc2f74081b5eb248f79170bc5e81ff1cf711a
Tristan Yang <yanggc888@163.com>
2022-11-22 16:32:28 +0800

    fix: remove swagger path cache

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -33,7 +33,7 @@ registerRoute(
       return false;
     } // If this is a URL that starts with /_, skip.
 
-    if (url.pathname.startsWith("/_")) {
+    if (url.pathname.startsWith("/_") || url.pathname.startsWith("/api/swagger")) {
       return false;
     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
