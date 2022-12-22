53b883d4a1453157f35d06a53c45acc540836f55
hdsuperman <zhd.superman@gmail.com>
2022-12-22 18:41:31 +0800

    chore: code style

--- src/routes/lazy.tsx
+++ src/routes/lazy.tsx
@@ -1,13 +1,16 @@
-import { PropsWithChildren, Suspense } from 'react';
+import {  ReactNode, Suspense, FC } from "react";
 import Loading from "../common/component/Loading";
-type Props = {}
 
-const Lazy = ({ children }: PropsWithChildren<Props>) => {
-    return (
-        <Suspense fallback={<Loading />}>
-            {children}
-        </Suspense>
-    );
+type Props = {
+  children?: ReactNode;
+}
+
+const Lazy: FC<Props> = ({ children }) => {
+  return (
+    <Suspense fallback={<Loading />}>
+      {children}
+    </Suspense>
+  );
 };
 
 export default Lazy;
