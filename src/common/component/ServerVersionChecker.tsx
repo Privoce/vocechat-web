bc882e786f64e03eadf432e4207dea608afba0f1
Tristan Yang <yanggc888@163.com>
2023-01-19 17:17:14 +0800

    chore: server version check

--- src/common/component/ServerVersionChecker.tsx
+++ src/common/component/ServerVersionChecker.tsx
@@ -14,7 +14,7 @@ const ServerVersionChecker = ({ empty = false, version, children }: Props) => {
   const { data: currentVersion, isSuccess } = useGetServerVersionQuery();
   if (!isSuccess) return null;
   const res = compareVersion(currentVersion, version);
-  if (res < 0) return empty ? null : <div className='flex flex-col gap-2 items-start border border-solid border-orange-500 p-3 rounded-lg'>
+  if (res < 0) return empty ? null : <div className='flex flex-col gap-2 items-start border border-solid border-orange-500 p-3 rounded-lg w-fit'>
     <span className='text-gray-400 text-sm'>
       <Trans i18nKey={"server_update.version_needed"}>
         <strong className='font-bold'>{{ version }}</strong>
