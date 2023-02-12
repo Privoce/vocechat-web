e1fb1a929ae64b8382afac10a1fe102c3407c5b9
Tristan Yang <yanggc888@163.com>
2023-02-13 07:39:03 +0800

    fix: tab broadcast error at local

--- src/common/hook/useTabBroadcast.ts
+++ src/common/hook/useTabBroadcast.ts
@@ -6,11 +6,10 @@ type ChannelData = {
     type: "NEW_TAB",
     message: string
 }
-const channel = new BroadcastChannel('VOCECHAT_CHANNEL');
 const useTabBroadcast = () => {
-    // const { stopStreaming } = useStreaming();
     const [tabActive, setTabActive] = useState(true);
     useEffect(() => {
+        const channel = new BroadcastChannel('VOCECHAT_CHANNEL');
         channel.postMessage({ type: "NEW_TAB", message: "new tab opened" } as ChannelData);
         const handler = (data: ChannelData) => {
             console.log("channel data", data);
