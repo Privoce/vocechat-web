7a53a3af86c373330575fd6412d00f36b7b52554
Tristan Yang <yanggc888@163.com>
2023-02-15 10:03:17 +0800

    tweak: price type

--- src/types/common.ts
+++ src/types/common.ts
@@ -3,14 +3,14 @@ export interface EntityId {
 }
 export type Theme = "auto" | "dark" | "light";
 export type AuthType = "register" | "login";
-export type PriceType = "subscription" | "payment";
+export type PriceType = "subscription" | "payment" | "booking";
 export type PriceSubscriptionDuration = "month" | "quarter" | "year";
 export type Price = {
-  title: string,
-  price: string,
-  limit: number,
-  pid: string,
-  desc: string,
+  title?: string,
+  price?: string,
+  limit?: number,
+  pid?: string,
+  desc?: string,
   type: PriceType,
   sub_dur?: PriceSubscriptionDuration
 }
