50cbff444c133bfabc14d68eec471f30a804bbb1
Tristan Yang <yanggc888@163.com>
2023-02-27 20:48:35 +0800

    feat: add faq to invite link

--- src/common/component/InviteLink.tsx
+++ src/common/component/InviteLink.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 import useInviteLink from "../hook/useInviteLink";
 import Input from "./styled/Input";
 import Button from "./styled/Button";
-import { useTranslation } from "react-i18next";
 import QRCode from "./QRCode";
+import IconQuestion from '../../assets/icons/question.svg';
 
 type Props = {};
 const InviteLink: FC<Props> = () => {
@@ -15,7 +16,12 @@ const InviteLink: FC<Props> = () => {
 
   return (
     <div className="flex flex-col items-start pb-8">
-      <span className="font-semibold text-sm mb-2 text-gray-500 dark:text-gray-50">{t("share_invite_link")}</span>
+      <p className="font-semibold text-sm mb-2 text-gray-500 dark:text-gray-50 flex gap-4">
+        {t("share_invite_link")}
+        <a className="text-primary-500 flex gap-1 items-center" href="http://doc.voce.chat/faq#fe_url" target="_blank" rel="noopener noreferrer">
+          <IconQuestion /> {t("invite_link_faq")}
+        </a>
+      </p>
       <div className="w-full md:w-[512px] mb-3 relative">
         <Input readOnly className={"large !pr-16"} placeholder="Generating" value={link} />
         <Button onClick={copyLink} className="ghost small border_less absolute right-1 top-1/2 -translate-y-1/2">
