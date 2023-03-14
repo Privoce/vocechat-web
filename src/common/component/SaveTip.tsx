f993b32051ab9867e1e66ef864faf250448f00a9
Tristan Yang <yanggc888@163.com>
2023-03-14 22:03:34 +0800

    fix: save tip z-index

--- src/common/component/SaveTip.tsx
+++ src/common/component/SaveTip.tsx
@@ -13,7 +13,9 @@ const SaveTip: FC<Props> = ({ saveHandler, resetHandler }) => {
   const { t } = useTranslation("setting");
   // const btnClass=clsx("")
   return (
-    <div className="z-50 w-full p-2 absolute bottom-4 md:bottom-16 left-0 flex flex-col md:flex-row items-center justify-between font-semibold text-gray-700 border border-solid border-gray-200 dark:border-gray-400 bg-white dark:bg-gray-600 shadow-xl rounded-full">
+    <div className="z-[999] w-full p-2 absolute bottom-4 md:bottom-16 left-0 
+    flex flex-col md:flex-row items-center justify-between font-semibold text-gray-700 border 
+    border-solid border-gray-200 dark:border-gray-400 bg-white dark:bg-gray-600 shadow-2xl dark:shadow-primary-400/50 rounded-full">
       <span className="p-2 text-sm dark:text-gray-200">{t('save_tip')}</span>
       <div className="flex items-center gap-3">
         <Button className="small ghost border_less !text-gray-700 !shadow-none dark:!text-gray-100" onClick={resetHandler}>
