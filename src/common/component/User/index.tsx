ecfa3b8f24746ba0c74ad828eed8f76ff1e75eb6
Tristan Yang <yanggc888@163.com>
2023-03-24 09:34:31 +0800

    chore: tweak onlin status circle

--- src/common/component/User/index.tsx
+++ src/common/component/User/index.tsx
@@ -45,9 +45,9 @@ const User: FC<Props> = ({
   const online = curr.online || curr.uid == loginUid;
   const containerClass = clsx(`relative flex items-center justify-start gap-2 rounded-lg select-none`, interactive && "md:hover:bg-gray-500/10", compact ? "p-0" : "p-2");
   const nameClass = clsx(`text-sm text-gray-500 max-w-[190px] truncate font-semibold dark:text-white`);
-  const statusClass = clsx(`absolute -bottom-0.5 -right-1.5 w-3 h-3 box-content rounded-full border-[2px] border-solid border-white dark:border-gray-300`,
+  const statusClass = clsx(`absolute -bottom-[2.5px] -right-[2.5px] border-content rounded-full border-[1px] border-solid border-white dark:border-gray-300`,
     online ? "bg-green-500" : "bg-zinc-400",
-    compact && "w-3.5 h-3.5");
+    compact ? "w-[15px] h-[15px]" : "w-3 h-3");
   if (!popover)
     return (
       <ContextMenu
@@ -79,7 +79,7 @@ const User: FC<Props> = ({
             </span>
           )}
           {owner && <IconOwner />}
-          {curr.is_bot && <IconBot className={clsx(compact && "absolute -top-1 -right-1", "!w-4 !h-4")} />}
+          {curr.is_bot && <IconBot className={clsx(compact && "absolute -top-[2.5px] -right-[2.5px]", "!w-[15px] !h-[15px]")} />}
         </div>
       </ContextMenu>
     );
