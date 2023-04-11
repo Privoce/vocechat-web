390e2e5ac9a630c61c5a8d27ea51f894adfdd84e
Tristan Yang <yanggc888@163.com>
2023-04-12 07:41:57 +0800

    refactor: markdown input css

--- src/common/component/MarkdownEditor/index.tsx
+++ src/common/component/MarkdownEditor/index.tsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, FC } from "react";
-import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import "prismjs/themes/prism.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
 import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js";
