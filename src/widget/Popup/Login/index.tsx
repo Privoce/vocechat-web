eb6cf610a8cd997fbd9759f51f70a505a73b8a9c
Tristan Yang <yanggc888@163.com>
2023-07-14 17:36:37 +0800

    feat: replace name with email in widget reg

--- src/widget/Popup/Login/index.tsx
+++ src/widget/Popup/Login/index.tsx
@@ -26,6 +26,7 @@ const Login = () => {
   const { clientId } = useGoogleAuthConfig();
   const { config: githubAuthConfig } = useGithubAuthConfig();
   const [register, { isLoading, isSuccess, data, error }] = useRegisterMutation();
+  //  const [login]= useLoginMutation();
   const { data: loginConfig, isSuccess: loginConfigSuccess } = useGetLoginConfigQuery();
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
@@ -38,12 +39,14 @@ const Login = () => {
     const data = new FormData(form);
     const name = data.get("username") as string;
     // const email = data.get("email") as string;
-    console.log("name,email", name);
-    const randomEmail = `${name}-${(Math.random() + 1).toString(36).substring(7)}@${from}`;
+    console.log("name", name);
+    const rand = (Math.random() + 1).toString(36).substring(7);
+    const nameFromEmail = `${name}-${rand}`;
+    const randomEmail = `${name}-${rand}-${from}`;
     register({
-      name,
+      name: nameFromEmail,
       email: randomEmail,
-      password: randomEmail
+      password: nameFromEmail
     });
     // const content = new FormData(form).get("prompt") as string;
   };
@@ -58,6 +61,11 @@ const Login = () => {
       switch (error.status) {
         case 409:
           toast.error("Email existed already!");
+          // login({
+          //   name: data?.name,
+          //   email: data?.email,
+          //   password
+          // })
           break;
         default:
           toast.error("Something error!");
@@ -73,8 +81,8 @@ const Login = () => {
     <div className="w-full max-w-[288px] flex flex-col gap-2 mt-4 animate-[fadeInUp_.5s_.8s_ease-in-out_both]">
       <div className="bg-white dark:bg-gray-700 border dark:border-gray-500 rounded-lg">
         <form className="px-4 py-3 flex flex-col gap-2" onSubmit={handleSubmit}>
-          <Input required placeholder="Name" type="text" name="username" />
-          {/* <Input required placeholder="Email" type="email" name='email' /> */}
+          {/* input email as username */}
+          <Input required placeholder="Email" type="email" name="username" />
           <StyledButton
             disabled={isLoading}
             type="submit"
