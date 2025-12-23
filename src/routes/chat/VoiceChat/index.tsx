a1a2d34f909a96baa5a6b32af96e342fd83cbe79
Will Sheng <92167095+syf20020816@users.noreply.github.com>
2025-12-23 19:29:49 +0800

    增加检测vocespace环境部署状态，修改部分翻译 (#280)
    translation: Vocespace -> VoceSpace
    - Download shell to deploy vocespace
    - check vocespace state

--- src/routes/chat/VoiceChat/index.tsx
+++ src/routes/chat/VoiceChat/index.tsx
@@ -104,7 +104,13 @@ const VoiceChat = ({ id, context = "channel" }: Props) => {
 
   const handleSendVocespaceRequest = async () => {
     if (vocespaceConfig && vocespaceConfig.state && vocespaceConfig.state === "undeployed") {
-      await sendVoceSpaceMsg(`https://vocespace.com/${context}_${id}`);
+      const genUUID = () =>
+        "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+          (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+        );
+      await sendVoceSpaceMsg(
+        `https://vocespace.com/${context}_${id}_${encodeURIComponent(genUUID())}`
+      );
     } else if (vocespaceConfig && vocespaceConfig?.enabled && vocespaceConfig.state === "success") {
       let url = vocespaceConfig.url;
       if (url.includes(":7880")) {
